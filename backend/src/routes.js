244727a2cd4e29cc48a00caaa0c2e3ea701f0249
Rafael Motta <rafaelmotta.ev@hotmail.com>
2020-01-15 07:17:43 -0300

    aula 1 - 1:13:40

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,34 @@
 const { Router } = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
-routes.post('/users', (request, response) => {
-    console.log(request.body);
-    return response.json({ message: 'Hello word' });
+    //async digo que a api pode demorar para responder
+routes.post('/devs', async(request, response) => {
+    const { github_username, techs, latitude, longitude } = request.body;
+    // await digo que deve esperar a resposta para seguir com o codigo
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    
+    let {name = login, avatar_url, bio } = apiResponse.data;
+    
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+    }
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+    });
+
+    return response.json(dev);
 });
 
 module.exports = routes;
