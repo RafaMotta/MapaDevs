3813add63c1f96469c74ab7e83a9e11a9abfca04
Rafael Motta <rafaelmotta.ev@hotmail.com>
2020-01-16 07:53:13 -0300

    aula 2 52:00

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,37 @@
-import React, { useState } from 'react';
-import Header from './Header';
+import React from 'react';
+
+import './global.css';
+import './App.css'
 //Componente: função(bloco) que retorna algum conteudo html, css, javascript no qual nao interfere no restante da aplicação
 //Estado: Informações mantidas pelo componente (Lembrar: imutabilidade(ESTUDAR CONCEITO! IMPORTANTE))
 //Propriedade: Informação que o componente PAI passa para o componente FILHO
 
 
 function App() {//Componente (PRIMEIRA LETRA SEMPRE MAIUSCULA)
-  let [counter, setCounter] = useState(0);
-  function incrementCounter(){
-    setCounter(counter + 1);
-  }
+  
   return (
-    //tag abaixo chama fragment que serve para nao atrapalhar o style do resto da aplicação
-    <> 
-      <h1>Contador {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+          <form>
+            <div className="input-block">
+              <label htmlFor="github_username">Usuário do Github</label>
+              <input name="github_username" id="github_username" required/>
+            </div>
+            
+            <div className="input-block"></div>
+              <label htmlFor="techs">Tecnologias</label>
+              <input name="techs" id="techs" required/>
+            </div>
+
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required/>
+          </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
