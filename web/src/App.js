adb701b7778bfa7f6dc2f5fe41e30ace057a7465
Rafael Motta <rafaelmotta.ev@hotmail.com>
2020-01-22 19:24:18 -0300

    MapaDevs - backend/Front(Web)
    WIP

--- web/src/App.js
+++ web/src/App.js
@@ -1,40 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import api from './services/api';
 
-
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
 //Componente: função(bloco) que retorna algum conteudo html, css, javascript no qual nao interfere no restante da aplicação
 //Estado: Informações mantidas pelo componente (Lembrar: imutabilidade(ESTUDAR CONCEITO! IMPORTANTE))
 //Propriedade: Informação que o componente PAI passa para o componente FILHO
 
-
 function App() {//Componente (PRIMEIRA LETRA SEMPRE MAIUSCULA)
   const [ devs, setDevs ] = useState([]);
-  const [ latitude, setLatitude ] = useState('');
-  const [ longitude, setLongitude ] = useState('');
-  const [ github_username, setGithubUsername ] = useState('');
-  const [ techs, setTechs ] = useState('');
-
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000,
-      }
-    );
-  },[]);
 
   useEffect(() => {
     async function loadDevs(){
@@ -45,92 +25,24 @@ function App() {//Componente (PRIMEIRA LETRA SEMPRE MAIUSCULA)
     loadDevs();
   },[]);
 
-  async function handleAddDev(e){
-    e.preventDefault();
+  async function handleAddDev(data){
     
-    const response = await api.post('./devs', {
-      github_username,
-      techs,
-      latitude,
-      longitude      
-    });
-
-    setGithubUsername('');
-    setTechs('');
-    setLatitude('');
-    setLongitude('');
+    const response = await api.post('./devs', data);
+    
+    setDevs([...devs, response.data]);
   }
  
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-          <form onSubmit={handleAddDev}>
-            <div className="input-block">
-              <label htmlFor="github_username">Usuário do Github</label>
-              <input 
-                name="github_username" 
-                id="github_username" 
-                required
-                value={github_username}
-                onChange={e => setGithubUsername(e.target.value)}
-              />
-            </div>
-            
-            <div className="input-block">
-              <label htmlFor="techs">Tecnologias</label>
-              <input 
-                name="techs" 
-                id="techs" 
-                required
-                value={techs}
-                onChange={e => setTechs(e.target.value)}
-              />
-            </div>
-
-            <div className="input-group">
-              <div className="input-block">
-                <label htmlFor="latitude">Latitude</label>
-                <input 
-                  type="number" 
-                  name="latitude" 
-                  id="latitude"
-                  value={latitude} 
-                  required
-                  onChange={e => setLatitude(e.target.value)}
-                />
-              </div>
-
-              <div className="input-block">
-                <label htmlFor="longitude">Longitude</label>
-                <input 
-                  type="number" 
-                  name="longitude" 
-                  id="longitude" 
-                  value={longitude} 
-                  required
-                  onChange={e => setLongitude(e.target.value)}
-                />
-              </div>
-            </div>
-            
-            <button type="submit">Salvar</button>
-          </form>
+          <DevForm onSubmit={handleAddDev} />
       </aside>
+      
       <main>
         <ul>
           {devs.map(dev => (
-            <li key={dev._id} className="dev-item">
-              <header>
-                <img src={dev.avatar_url} alt={dev.name} />
-                <div className="user-info">
-                  <strong>{dev.name}</strong>
-                  <span>{dev.techs.join(', ')}</span>
-                </div>
-              </header>
-              <p>{dev.bio}</p>
-              <a href={`https://github.com/${dev.github_username}`}>Acessar Perfil Github</a>
-            </li>   
+            <DevItem key={dev._id} dev={dev} />
           ))}
               
         </ul>
