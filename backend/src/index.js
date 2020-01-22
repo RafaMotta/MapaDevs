7b957a98cbc5430ba5ee668dbbbd6a941354b23e
Rafael Motta <rafaelmotta.ev@hotmail.com>
2020-01-22 19:28:14 -0300

    WIP

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://rafaelmotta:MpQidoT6f98eLLWe@cluster0-pnhxj.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://rafaelmotta:ASDADASD@cluster0-pnhxj.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
