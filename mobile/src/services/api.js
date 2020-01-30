f4ce359f5c9e8edee724d9147d37014e3f4790d9
Rafael Motta <rafaelmotta.ev@hotmail.com>
2020-01-30 07:06:25 -0300

    ajustes

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     //localhost do expo:porta usada no backend
-    baseURL: 'http://192.168.1.54:3333'
+    baseURL: 'http://locahost:3333'
 });
 
 export default api;
